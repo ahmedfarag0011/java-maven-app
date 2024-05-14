1335a8f2ce19b79cef2a296003e9a8eda4f6a076
ahmedfarag0011 <163968879+ahmedfarag0011@users.noreply.github.com>
2024-05-14 11:24:41 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,12 +5,14 @@ def buildJar() {
 
 def buildImage() {
     echo "Building the Docker image.."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+                    // Use withCredentials block properly
         sh 'docker build -t ahmedfarag0011/my-repo:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh "docker push ahmedfarag0011/my-repo:jma-2.0"
     }
 }
+
 
 def deployApp() {
     echo "Deploying the application.."
